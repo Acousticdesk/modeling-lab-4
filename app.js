cbac4c6f250d1dae2d4aa9aeb5ba08d371211d8d
Andrii Kicha <akicha@star.global>
2022-11-11 14:57:45 +0200

    Add UI for parameters and input customization

--- app.js
+++ app.js
@@ -1,107 +1,144 @@
 // Story:
-// Mechanical keyboards: 2000
+// Mechanical keyboards business: initial price - 1000 UAH
 const randomSign = () => Math.random() > 0.5 ? 1 : -1;
 const getExponentialRandomForDeviation = (t) => randomSign() * Math.random() * Math.exp(t)
-// const getRandom = (t) => randomSign() * t * Math.random()
 
 // Weight coefficient
 // The model with learning, but close to the highest boundary i.e. 1
-const p = 0.5;
+// Is defined in the requirements in the LAB 4
+const p = 0.9;
 
-// coefficients of linear regression
-const A = 1;
-// Should be higher than K
-const B = 2;
+let ut = [];
+let prices = [];
+let DResults = [];
+let SResults = [];
 
-const C = 1;
-const K = 1;
+let chart = null;
+let priceChart = null;
 
-const numIterations = 25
-
-// UAH
-const initialPrice = 2000;
-const prices = [initialPrice, initialPrice + 150, initialPrice - 250];
-const ut = [];
+let inputData;
+let parametersData;
 
 // Demand
 // x: price
 // t: iteration
 function D(t) {
+  const { A, B } = parametersData;
   ut[t] = ut[t] || getExponentialRandomForDeviation(t);
   return A - (B * Math.pow(prices[t], 2)) + ut[t]
 }
 
 // Supply
 function S(t) {
+  const { C, K } = parametersData;
   ut[t] = ut[t] || getExponentialRandomForDeviation(t);
   // when t = 0 we still don't have the t - 1 price, let's assume it is 0
   const Xp = (prices[prices.length - 2] || 0) - p * ((prices[prices.length - 2] || 0) - (prices[prices.length - 3] || 0));
   return C + K * Xp + getExponentialRandomForDeviation(t)
 }
 
-const DResults = [D(0)];
-const SResults = [S(0)];
-
 function findNextStepPrice(t) {
+  const { A, B } = parametersData;
   ut[t] = ut[t] || getExponentialRandomForDeviation(t);
   const nextPrice = -((SResults[t - 1] - A - ut[t]) / B);
+
+  // important
   return nextPrice > 0 ? Math.sqrt(nextPrice) : 0;
 }
 
-for (let t = 1; t < numIterations; t += 1) {
-  if (t > 2) {
-    prices.push(findNextStepPrice(t));
+function model() {
+  const { num_iterations: numIterations, initial_price: initialPrice } = inputData;
+
+  ut = [];
+  prices = [initialPrice, initialPrice + 150, initialPrice - 250];
+
+  DResults = [D(0)];
+  SResults = [S(0)];
+
+  for (let t = 1; t < numIterations; t += 1) {
+    if (t > 2) {
+      prices.push(findNextStepPrice(t));
+    }
+    DResults.push(D(t));
+    SResults.push(S(t));
   }
-  DResults.push(D(t));
-  SResults.push(S(t));
+
+  const labels = (new Array(numIterations)).fill(0).map((_, index) => index);
+
+  const config = {
+    type: "line",
+    data: {
+      labels,
+      datasets: [
+        {
+          label: 'Demand',
+          backgroundColor: 'red',
+          borderColor: 'red',
+          data: DResults,
+        },
+        {
+          label: 'Supply',
+          backgroundColor: 'blue',
+          borderColor: 'blue',
+          data: SResults,
+        },
+      ],
+    },
+    options: {},
+  };
+
+  const config2 = {
+    type: "line",
+    data: {
+      labels,
+      datasets: [
+        {
+          label: 'Price',
+          backgroundColor: 'green',
+          borderColor: 'green',
+          data: prices,
+        }
+      ],
+    },
+    options: {},
+  };
+
+  if (chart) {
+    chart.destroy();
+    chart = null;
+  }
+
+  chart = new Chart(
+    document.getElementById("chart"),
+    config
+  );
+
+  if (priceChart) {
+    priceChart.destroy();
+    priceChart = null;
+  }
+
+  priceChart = new Chart(
+    document.getElementById("chart_price"),
+    config2
+  );
 }
 
-const labels = (new Array(numIterations)).fill(0).map((_, index) => index);
+// forms
+const submitButton = window.form_submit;
+const formInput = window.model_input_form;
+const formParameters = window.model_parameters_form;
 
-const config = {
-  type: "line",
-  data: {
-    labels,
-    datasets: [
-      {
-        label: 'Demand',
-        backgroundColor: 'red',
-        borderColor: 'red',
-        data: DResults,
-      },
-      {
-        label: 'Supply',
-        backgroundColor: 'blue',
-        borderColor: 'blue',
-        data: SResults,
-      },
-    ],
-  },
-  options: {},
-};
+function serializeForm(formElement) {
+  const formEntries = Array.from(new FormData(formElement));
+  const formEntriesNormalized = formEntries.map(([key, value]) => [key, Number(value)]);
 
-const config2 = {
-  type: "line",
-  data: {
-    labels,
-    datasets: [
-      {
-        label: 'Price',
-        backgroundColor: 'green',
-        borderColor: 'green',
-        data: prices,
-      }
-    ],
-  },
-  options: {},
-};
+  return Object.fromEntries(formEntriesNormalized)
+}
 
-new Chart(
-  document.getElementById("chart"),
-  config
-);
+submitButton.addEventListener('click', () => {
+  inputData = serializeForm(formInput);
+  parametersData = serializeForm(formParameters);
 
-new Chart(
-  document.getElementById("chart_price"),
-  config2
-);
+  model();
+});
